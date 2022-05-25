47c22782fac3580ca90742e1e6f38ce4e9a9c171
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-24 19:44:19 -0700

    commit tree cc4ca9e53afd405cf93ca76ecb2a626fcd7927a0

--- main.js
+++ main.js
@@ -4,7 +4,30 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heartButton = document.querySelectorAll(".like-glyph");
 
+heartButton.forEach(element => {
+  element.addEventListener("click", (e) => {
+    mimicServerCall()
+    .then(res => {
+      if (element.innerText === EMPTY_HEART){
+        element.innerText = FULL_HEART;
+        element.classList.add("activated-heart");
+      }
+      else {
+        element.innerText = EMPTY_HEART;
+        element.classList.remove("activated-heart");
+      }
+    })
+    .catch((error) => {
+      const errorModal = document.getElementById("modal");
+      errorModal.classList.remove("hidden");
+      const errorPara = document.getElementById("modal-message");
+      errorPara.innerText = error;
+      setTimeout(() => errorModal.classList.add("hidden"), 3000);
+    });
+  })
+})
 
 
 //------------------------------------------------------------------------------
